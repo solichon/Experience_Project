0e57f7403f0c91ae73fb0170c06760645df3df22
Eric Boutueil <34768473+EricBoutueil@users.noreply.github.com>
2018-02-28 15:02:12 +0100

    Update timeslot.rb
    added has many validations

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,5 +1,7 @@
 class Timeslot < ApplicationRecord
   belongs_to :activity
+  has_many :bookings
+  has_many :clients, through: :bookings
 
   validates :start_datetime, presence: true
   validates :end_datetime, presence: true

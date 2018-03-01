12de3f97ff9482b0953bf985deca9d5a7267b66f
Eric Boutueil <e.boutueil@gmail.com>
2018-03-01 16:13:56 +0100

    timeslots-index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :timeslots, only: [ :index ]
+
   devise_for :users
-  root to: 'pages#home'
+
+  root to: 'timeslots#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

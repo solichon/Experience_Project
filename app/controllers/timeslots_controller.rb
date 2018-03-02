8dd9f6bf5b482b590be914979d0e07295ea839d0
Eric Boutueil <e.boutueil@gmail.com>
2018-03-02 15:05:08 +0100

    timeslots-create-new

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -2,4 +2,28 @@ class TimeslotsController < ApplicationController
   def index
     @timeslots = Timeslot.all
   end
+
+    def new
+    @timeslot = Timeslot.new
+
+  end
+
+  def create
+    @timeslot = Timeslot.new(timeslot_params)
+    @timeslot.status = "empty"
+    binding.pry
+    activity = Activity.find(timeslot_params[:activity_id])
+    @timeslot.end_datetime = @timeslot.start_datetime + activity.duration_in_minutes * 60
+    if @timeslot.save
+      redirect_to timeslots_path
+    else
+      render :new
+    end
+  end
+
+    private
+
+  def timeslot_params
+    params.require(:timeslot).permit(:start_datetime, :end_datetime, :activity_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'timeslots#index'
-  
-  resources :timeslots, only: [ :index ]
+
+  resources :timeslots, only: [ :index, :create, :new ]
   resources :activities, only: [ :new, :create, :show ]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

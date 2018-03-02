f91b9fd764a9d646e5c12bc3b479a4121130e03b
celinepernod <celine.pernod@gmail.com>
2018-03-02 15:39:40 +0100

    homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @title = "A cool Home"
+  end
+
+  def about
+    @title = "A cool Home"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'timeslots#index'
 
+  get "home", to: "pages#home"
+  get "about", to: "pages#about"
   resources :timeslots, only: [ :index, :show ]
   resources :activities, only: [ :new, :create, :show ]
 

02fc12af6992a09c07ad1812fa2d5293f15a3d02
Eric Boutueil <34768473+EricBoutueil@users.noreply.github.com>
2018-03-07 16:30:39 +0100

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,6 @@ module ExperienceProject
     config.load_defaults 5.1
     config.i18n.default_locale = :fr
 
-    config.time_zone = 'Europe/Paris'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

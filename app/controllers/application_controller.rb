12cd1e18a4e5936ea7cf09e2fc62e3dcc7c8b70b
Samuel Olichon <samuel.olichon@gmail.com>
2018-03-07 08:33:32 +0100

    add Facebook Open Graph data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,3 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 end
+
+def default_url_options
+  { host: ENV["HOST"] || "localhost:3000" }
+end
